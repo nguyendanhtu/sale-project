197b7e78caa2d96330ca8027898e39a58e169332
toannm@bkindex.com <toannm@bkindex.com@7435eec9-0750-5c18-3d5b-9f584f7e763d>
2011-08-16 03:44:15 +0000

    Thêm DM_Product

--- trunk/SourceCode/SaleDS/CDBNames.cs
+++ trunk/SourceCode/SaleDS/CDBNames.cs
@@ -16,6 +16,19 @@ namespace Sale.CDBNames
         public const string CATEGORY_DESCRIPTION = "CATEGORY_DESCRIPTION";
     }
 
+    public class DM_PRODUCT
+    {
+        public const string ID = "ID";
+        public const string PRODUCT_CODE = "PRODUCT_CODE";
+        public const string PRODUCT_NAME = "PRODUCT_NAME";
+        public const string UNIT_ID = "UNIT_ID";
+        public const string CATEGORY_ID = "CATEGORY_ID";
+        public const string IMAGE_PATH = "IMAGE_PATH";
+        public const string DESCRIPTION = "DESCRIPTION";
+        public const string PROVIDER_ID = "PROVIDER_ID";
+        public const string CURRENT_PRICE = "CURRENT_PRICE";
+    }
+
 }
 
 
