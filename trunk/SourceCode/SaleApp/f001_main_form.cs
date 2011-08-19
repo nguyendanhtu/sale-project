84bde53d4f1bce99852a89f2761758286712485f
uyvq@bkindex.com <uyvq@bkindex.com@7435eec9-0750-5c18-3d5b-9f584f7e763d>
2011-08-19 18:33:46 +0000

    UYvq add them giao dien cua form , chua add them tinh nang thay doi giao diện

---
+++ trunk/SourceCode/SaleApp/f001_main_form.cs
@@ -0,0 +1,297 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using DevExpress.XtraBars;
+
+
+using System.Collections;
+
+
+using IP.Core.IPCommon;
+using IP.Core.IPSystemAdmin;
+
+
+namespace SaleApp
+{
+    public partial class f001_main_form : DevExpress.XtraBars.Ribbon.RibbonForm
+    {
+        private IContainer components;
+
+        public f001_main_form()
+        {
+            //
+            // Required for Windows Form Designer support
+            //
+            InitializeComponent();
+
+            //
+            // TODO: Add any constructor code after InitializeComponent call
+            //
+            format_controls();
+        }
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (components != null)
+                {
+                    components.Dispose();
+                }
+            }
+            base.Dispose(disposing);
+        }
+
+      
+
+
+
+        #region public Interface
+        public void display(ref IPConstants.HowUserWantTo_Exit_MainForm o_exitMode)
+        {
+            this.ShowDialog();
+            o_exitMode = m_exitMode;
+        }
+
+        #endregion
+        #region Data Structures
+
+        #endregion
+
+        #region Members
+
+        IPConstants.HowUserWantTo_Exit_MainForm m_exitMode = IPConstants.HowUserWantTo_Exit_MainForm.ExitFromSystem;
+        #endregion
+
+        #region Private Methods
+        private void format_controls()
+        {
+            CControlFormat.setFormStyle(this);
+
+            set_define_events();
+        }
+
+        private void show_manager_user()
+        {
+            if (!CAppContext_201.IsHavingQuyen(IP.Core.IPSystemAdmin.PHAN_QUYEN.BACK_UP_AND_RESTORE))
+            {
+                BaseMessages.MsgBox_Infor(" Người sử dụng không được quyền truy nhập phần này !!! ");
+                return;
+            }
+            f999_ht_nguoi_su_dung v_frm_999 = new f999_ht_nguoi_su_dung();
+            v_frm_999.display();
+        }
+        private void show_introduction()
+        {
+            if (!CAppContext_201.IsHavingQuyen(IP.Core.IPSystemAdmin.PHAN_QUYEN.IN_BAO_CAO))
+            {
+                BaseMessages.MsgBox_Infor(" Người sử dụng không được phép truy nhập phần này !!! ");
+                return;
+            }
+            f996_license_form v_frm996 = new f996_license_form();
+            v_frm996.display();
+            //			IP.Core.IPSystemAdmin.f996_license v_frm = new f996_license () ;
+            //			v_frm.display () ;
+        }
+
+        private void show_tu_dien_phan_quyen()
+        {
+            //if (!CAppContext_201.IsHavingQuyen(IP.Core.IPSystemAdmin.PHAN_QUYEN.CHUC_NANG))
+            //{
+            //    BaseMessages.MsgBox_Infor(" Người sử dụng không có quyền truy nhập phần này ! ");
+            //    return;
+            //}
+            f100_TuDien v_frm = new f100_TuDien();
+            v_frm.ShowDialog();
+        }
+
+        private void set_inital_form_load()
+        {
+
+        }
+        private void show_category()
+        {
+            if (!CAppContext_201.IsHavingQuyen(IP.Core.IPSystemAdmin.PHAN_QUYEN.IN_BAO_CAO))
+            {
+                BaseMessages.MsgBox_Infor(" Người sử dụng không được phép truy nhập phần này !!! ");
+                return;
+            }
+            f201_dm_category v_frm201 = new f201_dm_category();
+            v_frm201.display();
+        }
+
+        private void show_product()
+        {
+            if (!CAppContext_201.IsHavingQuyen(IP.Core.IPSystemAdmin.PHAN_QUYEN.IN_BAO_CAO))
+            {
+                BaseMessages.MsgBox_Infor(" Người sử dụng không được phép truy nhập phần này !!! ");
+                return;
+            }
+
+            //f301_dm_product v_frm301 = new f301_dm_product();
+            //v_frm301.display();
+        }
+
+        /// <summary>
+        /// Show form danh mục quản lý đơn vị 
+        /// </summary>
+        private void show_unit_product()
+        {
+            if (!CAppContext_201.IsHavingQuyen(IP.Core.IPSystemAdmin.PHAN_QUYEN.IN_BAO_CAO))
+            {
+                BaseMessages.MsgBox_Infor(" Người sử dụng không được phép truy nhập phần này !!! ");
+                return;
+            }
+
+            f901_dm_unit v_frm901 = new f901_dm_unit();
+            v_frm901.display();
+        }
+        #endregion
+        //
+        //
+        //   event handlers
+        //
+        //
+        private void set_define_events()
+        {
+
+
+
+
+
+        }
+
+     
+      
+
+      
+
+      
+     
+    
+      
+
+        private void mnu_user_management_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            try
+            {
+                show_manager_user();
+
+            }
+            catch (Exception v_e)
+            {
+
+                CSystemLog_301.ExceptionHandle(v_e);
+            }
+
+        }
+
+        private void mnu_directory_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            try
+            {
+
+                show_tu_dien_phan_quyen();
+            }
+            catch (Exception v_e)
+            {
+
+                CSystemLog_301.ExceptionHandle(v_e);
+            }
+           
+        }
+
+        private void mnu_type_of_directory_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            try
+            {
+
+
+            }
+            catch (Exception v_e)
+            {
+
+                CSystemLog_301.ExceptionHandle(v_e);
+            }
+        }
+
+        private void mnu_exit_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            try
+            {
+                if (BaseMessages.MsgBox_Confirm("Bạn có thật sự muốn kết thúc chương trình không ? "))
+                {
+                    m_exitMode = IPConstants.HowUserWantTo_Exit_MainForm.ExitFromSystem;
+                    this.Close();
+                }
+            }
+            catch (Exception v_e)
+            {
+                CSystemLog_301.ExceptionHandle(v_e);
+            }
+        }
+
+        private void mnu_category_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            try
+            {
+                show_category();
+            }
+            catch (Exception v_e)
+            {
+
+                CSystemLog_301.ExceptionHandle(v_e);
+            }
+        }
+
+        private void mnu_product_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            try
+            {
+                show_product();
+            }
+            catch (Exception v_e)
+            {
+                CSystemLog_301.ExceptionHandle(v_e);
+            }
+        }
+
+        private void mnu_dm_unit_product_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            try
+            {
+                show_unit_product();
+            }
+            catch (Exception v_e)
+            {
+                CSystemLog_301.ExceptionHandle(v_e);
+            }
+        }
+
+        private void mnu_license_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            try
+            {
+
+                show_introduction();
+            }
+            catch (Exception v_e)
+            {
+
+                CSystemLog_301.ExceptionHandle(v_e);
+            }
+        }
+
+    
+
+    
+     
+    }
+}
