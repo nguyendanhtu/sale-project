43f03a83fc0fc6046e92a867a6d1b5f53afc0be5
linhdh <linhdh@bkindex.com>
2011-08-21 10:23:35 +0000

    Add thêm form nhập số dư đầu

---
+++ SourceCode/SaleApp/f321_nhap_so_du_hang_hoa.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SaleApp
+{
+    public partial class f321_nhap_so_du_hang_hoa : Form
+    {
+        public f321_nhap_so_du_hang_hoa()
+        {
+            InitializeComponent();
+        }
+    }
+}
