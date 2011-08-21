fc060de74e9085b660fe210600f5c0efc6bafdab
linhdh@bkindex.com <linhdh@bkindex.com@7435eec9-0750-5c18-3d5b-9f584f7e763d>
2011-08-21 10:44:00 +0000

    Đổi tên form produuct danh muc

--- trunk/SourceCode/SaleApp/f321_nhap_so_du_hang_hoa.cs
+++ trunk/SourceCode/SaleApp/f321_nhap_so_du_hang_hoa.cs
@@ -14,6 +14,31 @@ namespace SaleApp
         public f321_nhap_so_du_hang_hoa()
         {
             InitializeComponent();
+            set_define_events();
         }
+
+        #region Members
+
+        #endregion
+
+        #region Public Interfaces
+
+        #endregion
+
+        #region Private Methods
+        private void set_define_events()
+        {
+           
+        }
+        private void check_data_validate()
+        {
+ 
+        }
+        #endregion
+
+        #region Events
+
+        #endregion
+
     }
 }
