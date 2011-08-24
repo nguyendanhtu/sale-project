e82d6b9244d90aa81b7cfe5cd356c9ccc9524846
toannm <toannm@bkindex.com>
2011-08-24 07:32:49 +0000

    Thêm 2 file 332 và 333

--- SourceCode/SaleApp/f331_xuat_ban_hang.Designer.cs
+++ SourceCode/SaleApp/f331_xuat_ban_hang.Designer.cs
@@ -28,19 +28,245 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(f331_xuat_ban_hang));
+            this.label1 = new System.Windows.Forms.Label();
+            this.m_txt_customer = new System.Windows.Forms.TextBox();
+            this.m_lbl_customer = new System.Windows.Forms.Label();
+            this.m_cmd_add_customer = new System.Windows.Forms.Button();
+            this.m_pnl_out_place_dm = new System.Windows.Forms.Panel();
+            this.m_cmd_save = new SIS.Controls.Button.SiSButton();
+            this.m_cmd_exit = new SIS.Controls.Button.SiSButton();
+            this.m_fg = new C1.Win.C1FlexGrid.C1FlexGrid();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.m_pnl_out_place_dm.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.m_fg)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(821, 65);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "HÓA ĐƠN BÁN HÀNG";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // m_txt_customer
+            // 
+            this.m_txt_customer.Location = new System.Drawing.Point(187, 137);
+            this.m_txt_customer.Name = "m_txt_customer";
+            this.m_txt_customer.Size = new System.Drawing.Size(352, 20);
+            this.m_txt_customer.TabIndex = 0;
+            // 
+            // m_lbl_customer
+            // 
+            this.m_lbl_customer.AutoSize = true;
+            this.m_lbl_customer.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.m_lbl_customer.Location = new System.Drawing.Point(41, 141);
+            this.m_lbl_customer.Name = "m_lbl_customer";
+            this.m_lbl_customer.Size = new System.Drawing.Size(78, 16);
+            this.m_lbl_customer.TabIndex = 10;
+            this.m_lbl_customer.Text = "Khách hàng";
+            // 
+            // m_cmd_add_customer
+            // 
+            this.m_cmd_add_customer.Location = new System.Drawing.Point(564, 135);
+            this.m_cmd_add_customer.Name = "m_cmd_add_customer";
+            this.m_cmd_add_customer.Size = new System.Drawing.Size(32, 22);
+            this.m_cmd_add_customer.TabIndex = 1;
+            this.m_cmd_add_customer.Text = "+";
+            this.m_cmd_add_customer.UseVisualStyleBackColor = true;
+            // 
+            // m_pnl_out_place_dm
+            // 
+            this.m_pnl_out_place_dm.Controls.Add(this.m_cmd_save);
+            this.m_pnl_out_place_dm.Controls.Add(this.m_cmd_exit);
+            this.m_pnl_out_place_dm.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.m_pnl_out_place_dm.Location = new System.Drawing.Point(0, 557);
+            this.m_pnl_out_place_dm.Name = "m_pnl_out_place_dm";
+            this.m_pnl_out_place_dm.Padding = new System.Windows.Forms.Padding(4);
+            this.m_pnl_out_place_dm.Size = new System.Drawing.Size(821, 36);
+            this.m_pnl_out_place_dm.TabIndex = 10;
+            // 
+            // m_cmd_save
+            // 
+            this.m_cmd_save.AdjustImageLocation = new System.Drawing.Point(0, 0);
+            this.m_cmd_save.BtnShape = SIS.Controls.Button.emunType.BtnShape.Rectangle;
+            this.m_cmd_save.BtnStyle = SIS.Controls.Button.emunType.XPStyle.Default;
+            this.m_cmd_save.Dock = System.Windows.Forms.DockStyle.Right;
+            this.m_cmd_save.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.m_cmd_save.ImageIndex = 10;
+            this.m_cmd_save.Location = new System.Drawing.Point(641, 4);
+            this.m_cmd_save.Name = "m_cmd_save";
+            this.m_cmd_save.Size = new System.Drawing.Size(88, 28);
+            this.m_cmd_save.TabIndex = 0;
+            this.m_cmd_save.Text = "&Xuất excel";
+            // 
+            // m_cmd_exit
+            // 
+            this.m_cmd_exit.AdjustImageLocation = new System.Drawing.Point(0, 0);
+            this.m_cmd_exit.BtnShape = SIS.Controls.Button.emunType.BtnShape.Rectangle;
+            this.m_cmd_exit.BtnStyle = SIS.Controls.Button.emunType.XPStyle.Default;
+            this.m_cmd_exit.Dock = System.Windows.Forms.DockStyle.Right;
+            this.m_cmd_exit.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.m_cmd_exit.ImageIndex = 12;
+            this.m_cmd_exit.Location = new System.Drawing.Point(729, 4);
+            this.m_cmd_exit.Name = "m_cmd_exit";
+            this.m_cmd_exit.Size = new System.Drawing.Size(88, 28);
+            this.m_cmd_exit.TabIndex = 1;
+            this.m_cmd_exit.Text = "Thoát (Esc)";
+            // 
+            // m_fg
+            // 
+            this.m_fg.ColumnInfo = resources.GetString("m_fg.ColumnInfo");
+            this.m_fg.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.m_fg.Location = new System.Drawing.Point(0, 233);
+            this.m_fg.Name = "m_fg";
+            this.m_fg.Size = new System.Drawing.Size(821, 274);
+            this.m_fg.Styles = new C1.Win.C1FlexGrid.CellStyleCollection(resources.GetString("m_fg.Styles"));
+            this.m_fg.TabIndex = 8;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.groupBox1.Location = new System.Drawing.Point(0, 507);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(821, 50);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(556, 14);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Tổng số tiền";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(286, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Tổng số lượng";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(653, 13);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(154, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(403, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(106, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(187, 85);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(151, 20);
+            this.textBox3.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(41, 86);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 16);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Mã phiếu";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(187, 187);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(121, 20);
+            this.textBox4.TabIndex = 14;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(41, 188);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 16);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Giảm giá";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
             // 
             // f331_xuat_ban_hang
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(517, 354);
+            this.ClientSize = new System.Drawing.Size(821, 593);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.m_fg);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.m_pnl_out_place_dm);
+            this.Controls.Add(this.m_cmd_add_customer);
+            this.Controls.Add(this.m_txt_customer);
+            this.Controls.Add(this.m_lbl_customer);
+            this.Controls.Add(this.label1);
             this.Name = "f331_xuat_ban_hang";
-            this.Text = "F331- Xuat ban hang";
+            this.Text = "F331- Hoa don ban hang";
+            this.Load += new System.EventHandler(this.f331_xuat_ban_hang_Load_1);
+            this.m_pnl_out_place_dm.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.m_fg)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox m_txt_customer;
+        private System.Windows.Forms.Label m_lbl_customer;
+        private System.Windows.Forms.Button m_cmd_add_customer;
+        internal System.Windows.Forms.Panel m_pnl_out_place_dm;
+        internal SIS.Controls.Button.SiSButton m_cmd_save;
+        internal SIS.Controls.Button.SiSButton m_cmd_exit;
+        private C1.Win.C1FlexGrid.C1FlexGrid m_fg;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
     }
 }
--- SourceCode/SaleApp/f331_xuat_ban_hang.cs
+++ SourceCode/SaleApp/f331_xuat_ban_hang.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using IP.Core.IPCommon;
 
 namespace SaleApp
 {
@@ -14,6 +15,94 @@ namespace SaleApp
         public f331_xuat_ban_hang()
         {
             InitializeComponent();
+            format_control();
+            set_define_event();
         }
+
+
+        #region Public Interface
+
+        #endregion
+
+
+        #region Private Method
+        private void format_control()
+        {
+            CControlFormat.setFormStyle(this);
+            CControlFormat.setC1FlexFormat(m_fg);
+            this.KeyPreview = true;
+        }
+
+        private void set_define_event()
+        {
+            this.Load += new EventHandler(f331_xuat_ban_hang_Load);
+            m_cmd_save.Click += new EventHandler(m_cmd_save_Click);
+            m_cmd_exit.Click += new EventHandler(m_cmd_exit_Click);
+            m_cmd_add_customer.Click += new EventHandler(m_cmd_add_customer_Click);
+        }
+
+       
+        private void save_data()
+        {
+
+        }
+
+        #endregion
+
+
+        #region Members
+        #endregion
+
+
+        #region Data Structure
+        #endregion
+
+
+        #region Event
+
+        void f331_xuat_ban_hang_Load(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        void m_cmd_add_customer_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        void m_cmd_add_product_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        void m_cmd_exit_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        void m_cmd_save_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        void m_cmd_add_product_2_bill_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                save_data();
+            }
+            catch (Exception v_e)
+            {
+                CSystemLog_301.ExceptionHandle(v_e);
+            }
+        }
+
+
+        #endregion
+
+        
+
+
     }
 }
+
